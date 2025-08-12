01daf51060d0af02a40b44ee293b995a62a97cc2
amannnb16-a11y <amannnb16@gmail.com>
2025-08-12 15:31:24 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
-
 function expandImage(el) {
-  const imgSrc = el.querySelector("img").src;
-  const overlay = document.getElementById("overlay");
-  const overlayImg = document.getElementById("overlayImg");
-  overlayImg.src = imgSrc;
-  overlay.classList.add("show");
+  const img = el.querySelector('img');
+  const overlay = document.getElementById('overlay');
+  const overlayImg = document.getElementById('overlayImg');
+  overlayImg.src = img.src;
+  overlay.classList.add('show');
 }
 
 function hideOverlay() {
-  document.getElementById("overlay").classList.remove("show");
+  document.getElementById('overlay').classList.remove('show');
 }
 
 function toggleLightMode() {
-  document.body.classList.toggle("light-mode");
-}
-
-function showContact() {
-  document.getElementById("contactModal").classList.add("show");
-  document.getElementById("mainContent").classList.add("blurred");
-}
-
-function closeContact() {
-  document.getElementById("contactModal").classList.remove("show");
-  document.getElementById("mainContent").classList.remove("blurred");
+  document.body.classList.toggle('light-mode');
 }
